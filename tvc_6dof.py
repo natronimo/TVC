e494210402d9ceb988c80bb678e59d0f65ac56a4
natronimo <123428083+natronimo@users.noreply.github.com>
2023-08-09 15:42:21 -0400

    Update tvc_6dof.py

--- tvc_6dof.py
+++ tvc_6dof.py
@@ -1,6 +1,5 @@
 import numpy as np
 from numpy import linalg as LA
-import scipy as sp
 from scipy.spatial.transform import Rotation as rot
 from scipy.integrate import solve_ivp
 import control as ct
@@ -11,16 +10,15 @@ L = 2
 r = 0.2
 I_sp = 250
 time_step = 0.01
-duration = 10
 
-x = 0
-y = 2
-z = 3
+x = -10
+y = 10
+z = 50
 v_x = 0
 v_y = 0
 v_z = 0
 theta_x = 0
-theta_y = -0.2
+theta_y = 0
 theta_z = 0
 omega_x = 0
 omega_y = 0
@@ -34,6 +32,10 @@ I_z = lambda m: (1/2)*m*r**2
 x_ref = 0
 y_ref = 0
 z_ref = 0
+
+T_s_lim = 300
+T_z_lim = 2500
+M_z_lim = 100
 
 x = np.array([[x], [y], [z], [v_x], [v_y], [v_z], [theta_x], [theta_y], [theta_z], [omega_x], [omega_y], [omega_z], [m]])
 ref = [[x_ref], [y_ref], [z_ref], [0], [0], [0], [0], [0], [0], [0], [0], [0]]
@@ -80,7 +82,7 @@ def dxdt(t, y):
                      (M_z - (I_y(m) - I_x(m))*omega_x*omega_y)/I_z(m),
                      m_dot])
 
-for t in np.arange(0, duration, time_step):
+while x[2] > 0:
 
     m = x[12, 0]
 
@@ -113,11 +115,13 @@ for t in np.arange(0, duration, time_step):
     K, S, E = ct.lqr(A, B, Q, R)
     u = np.matmul(K, ref - x[0:12]) + [[0], [0], [m*g], [0]]
 
+    u[0, 0] = max(-T_s_lim, min(T_s_lim, u[0, 0]))
+    u[1, 0] = max(-T_s_lim, min(T_s_lim, u[1, 0]))
+    u[2, 0] = max(0, min(T_z_lim, u[2, 0]))
+    u[3, 0] = max(-M_z_lim, min(M_z_lim, u[3, 0]))
+
     x_history = np.append(x_history, x, 1)
     u_history = np.append(u_history, u, 1)
     
     sol = solve_ivp(dxdt, [0, time_step], np.transpose(x)[0], t_eval=[time_step])
     x = sol.y
-
-np.savetxt("x_history.csv", x_history)
-np.savetxt("u_history.csv", u_history)
